1ab023ec33addff6d2563300eae904fbc9dc9665
dotrafi <146179374+dotrafi@users.noreply.github.com>
2026-06-13 18:27:52 +0700

    Create PembayaranController.php

---
+++ PembayaranController.php
@@ -0,0 +1,18 @@
+<?php
+
+class PembayaranController
+{
+    public function upload(Request $request)
+    {
+        $file = $request->file('dokumen');
+
+        $filename = time() . '.pdf';
+
+        $file->move(public_path('dokumen'), $filename);
+
+        Pembayaran::create([
+            'dokumen' => $filename,
+            'status' => 'uploaded'
+        ]);
+    }
+}
